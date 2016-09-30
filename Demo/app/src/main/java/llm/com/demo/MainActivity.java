f33e848196b220de0653f8025f5cc13f8ffb37f6
theFallenLeaves <1611892276@qq.com>
2016-09-30 17:33:23 +0800

    Signed-off-by: theFallenLeaves <1611892276@qq.com>

--- Demo/app/src/main/java/llm/com/demo/MainActivity.java
+++ Demo/app/src/main/java/llm/com/demo/MainActivity.java
@@ -2,12 +2,15 @@ package llm.com.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView textView=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView= (TextView) findViewById(R.id.textView_main);
+        textView.setText("测试数据");
     }
 }
